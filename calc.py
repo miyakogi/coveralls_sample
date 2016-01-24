d087551f5bd22a237d7e8df5ca027f801ca0f5e8
miyakogi <miyako.dev@gmail.com>
2016-01-24 20:24:57 +0900

    Add non-tested function

--- calc.py
+++ calc.py
@@ -11,3 +11,12 @@ def calculator(op, a, b):
         return a * b
     elif op == '/':
         return a / b
+
+
+def some_func():
+    '''This function is not tested'''
+    do = 'something'
+    here = ...
+    for i in range(10):
+        b = i + 1
+        print(b)
